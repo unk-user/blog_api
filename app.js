27658bb878d3347c4329a6d708ca68d921ef5e53
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-24 21:26:59 +0000

    setup middleware

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const secretKey = process.env.KEY_SECRET;
+
+//require routes
+const homeRoute = require('./routes/home');
+const postsRoute = require('./routes/posts');
+const authorRoute = require('./routes/author');
+
+// loading environment variables
+dotenv.config();
+// connecting to database
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+  });
+
+const app = express();
+
+// setting middleware functions
+app.use(express.json());
+app.use('/', homeRoute);
+app.use('/posts', postsRoute);
+app.use('/author', authorRoute);
+
+
+//starting server
+app.listen(3000, () => {
+  console.log('server started on port 3000');
+});
