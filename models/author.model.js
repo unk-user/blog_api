fd9d992a283f65f6ad79c1712e8e1687628f5c0d
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-24 13:58:50 +0000

    set up author schema

---
+++ models/author.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Author', authorSchema);
