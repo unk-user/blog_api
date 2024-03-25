37c1997125480fba095dd9fb35a22005190aa588
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-25 10:02:26 +0000

    vercel

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const Post = require('../models/post.model');
+const Post = require('../../models/post.model');
 
 const router = express.Router();
 
 //get recently published posts
-router.get('/', async(req, res, next) => {
-  const posts = await Post.find({published: true}).sort({ createdAt: -1 }).limit(5);
+router.get('/', async (req, res, next) => {
+  const posts = await Post.find({ published: true })
+    .sort({ createdAt: -1 })
+    .limit(5);
   res.json({
     posts: posts,
-  })
+  });
 });
 
 module.exports = router;
