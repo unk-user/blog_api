13693a323942219e00bbf106f4ba603d40c30754
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-26 01:01:35 +0000

    debug

--- app.js
+++ app.js
@@ -27,12 +27,6 @@ const app = express();
 
 // setting middleware functions
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 app.use('/', homeRoute);
 app.use('/posts', postsRoute);
 app.use('/author', authorRoute);
